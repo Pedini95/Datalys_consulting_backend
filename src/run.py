5f8f7a21443b29164274fe011b33a5825e0bae98
kouadio <innocent.kouadio>
2025-05-16 02:12:36 +0000

    Condition for face

--- src/run.py
+++ src/run.py
@@ -2,4 +2,4 @@ from app import app
 import os 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 8081))
+    app.run(debug=False, host='0.0.0.0', port=os.getenv('PORT', 8081))
