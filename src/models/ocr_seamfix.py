93cd6bf8a42faefba39af8c9e07421fb3b66acba
kouadio <innocent.kouadio>
2025-05-20 19:33:33 +0000

    Condition for face

--- src/models/ocr_seamfix.py
+++ src/models/ocr_seamfix.py
@@ -6,24 +6,33 @@ from datetime import datetime, date
 import logging
 import utils.utilities as utilities
 
-class FacesMatching(db.Model):
-    __tablename__ = 'faces_matching'
 
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(255))
+class OCRSeamfix(db.Model):
+    __tablename__ = 'ocr_seamfix'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cardId = db.Column(db.String(255))
     msisdn = db.Column(db.String(255))
-    request = db.Column(db.Text)
-    response = db.Column(db.Text)
-    code = db.Column(db.String(255))
-    errors = db.Column(db.Text)
-    matchId = db.Column(db.String(255))
-    score = db.Column(db.Float)
+    nin = db.Column(db.String(255))
+    firstName = db.Column(db.String(255))
+    middleName = db.Column(db.String(255))
+    lastName = db.Column(db.String(255))
+    gender = db.Column(db.String(255))
+    birdDate = db.Column(db.String(255))
     status = db.Column(db.String(255))
-    transactionRef = db.Column(db.String(255))
+    expiry = db.Column(db.String(255))
+    placeOfBirth = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    documentCountryName = db.Column(db.String(255))
+    documentDescription = db.Column(db.String(255))
+    documentName = db.Column(db.String(255))
+    documentType = db.Column(db.String(255))
+    documentYear = db.Column(db.String(255))
+    photo = db.Column(db.Text)
     search_string = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
-    is_deleted = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    is_deleted = db.Column(db.Boolean)
 
 
     def as_dict(self):
@@ -39,35 +48,31 @@ class FacesMatching(db.Model):
     
     # Repository
     def find_one(id, is_deleted):
-        actions_logs = FacesMatching.query.filter_by(id=id, is_deleted=is_deleted).first()
-        return actions_logs
-
-    def find_by_libelle(libelle, is_deleted):
-        actions_logs = FacesMatching.query.filter_by(libelle=libelle, is_deleted=is_deleted).first()
-        return actions_logs
-
+        ocr_seamfix = OCRSeamfix.query.filter_by(id=id, is_deleted=is_deleted).first()
+        return ocr_seamfix
+        
     def save(self):
         db.session.add(self)
         db.session.commit()
-
+        
     def update(self):
         db.session.commit()
-
+        
     def delete(self):
         self.is_deleted = True
         self.updated_at = datetime.utcnow()
         db.session.commit()
-
+        
     @staticmethod
     def get_by_criteria(criteria, index, size):
-        query = FacesMatching.query
+        query = OCRSeamfix.query
 
         # Définir une liste de conditions
-        conditions = [FacesMatching.is_deleted == False]
+        conditions = [OCRSeamfix.is_deleted == False]
         if 'id' in criteria:
-            conditions.append(FacesMatching.id == criteria['id'])
+            conditions.append(OCRSeamfix.id == criteria['id'])
         if 'search_string' in criteria:
-            conditions.append(FacesMatching.search_string.like(f"%{criteria['search_string']}%"))
+            conditions.append(OCRSeamfix.search_string.like(f"%{criteria['search_string']}%"))
 
         # **Gestion du filtre par date**
         if 'date_param' in criteria:
@@ -86,27 +91,26 @@ class FacesMatching(db.Model):
             end_date_converted = convert_date(end_date)
             if start_date_converted or end_date_converted:
                 if operator == "[]" and start_date_converted and end_date_converted:
-                    # conditions.append(Registration.created_at.between(start_date_converted, end_date_converted))
-                    conditions.append(and_(FacesMatching.created_at >= start_date_converted, FacesMatching.created_at <= end_date_converted))
+                    conditions.append(and_(OCRSeamfix.created_at >= start_date_converted, OCRSeamfix.created_at <= end_date_converted))
                 elif operator == "][" and start_date_converted and end_date_converted:
-                    conditions.append(FacesMatching.created_at.not_between(start_date_converted, end_date_converted))
+                    conditions.append(OCRSeamfix.created_at.not_between(start_date_converted, end_date_converted))
                 elif operator == ">" and start_date_converted:
-                    conditions.append(FacesMatching.created_at > start_date_converted)
+                    conditions.append(OCRSeamfix.created_at > start_date_converted)
                 elif operator == "<" and end_date_converted:
-                    conditions.append(FacesMatching.created_at < end_date_converted)
+                    conditions.append(OCRSeamfix.created_at < end_date_converted)
                 elif operator == ">=" and start_date_converted:
-                    conditions.append(FacesMatching.created_at >= start_date_converted)
+                    conditions.append(OCRSeamfix.created_at >= start_date_converted)
                 elif operator == "<=" and end_date_converted:
-                    conditions.append(FacesMatching.created_at <= end_date_converted)
+                    conditions.append(OCRSeamfix.created_at <= end_date_converted)
                 elif operator == "==" and start_date_converted:
-                    conditions.append(FacesMatching.created_at == start_date_converted)
+                    conditions.append(OCRSeamfix.created_at == start_date_converted)
                 elif operator == "!=" and start_date_converted:
-                    conditions.append(FacesMatching.created_at != start_date_converted)
+                    conditions.append(OCRSeamfix.created_at != start_date_converted)
 
         # Appliquer toutes les conditions à la requête
         query = query.filter(and_(*conditions))
         # Ajouter l'ordre de tri par ID décroissant
-        query = query.order_by(FacesMatching.id.desc())
+        query = query.order_by(OCRSeamfix.id.desc())
         # Ajouter la pagination
         total_items = query.count()
         query = query.offset(index * size).limit(size)
