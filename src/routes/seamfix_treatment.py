a78229e8a4a1981816ddf327b5ccb8834a4df846
kouadio <innocent.kouadio>
2025-06-06 10:03:25 +0000

    MAJ: gestion des logs

--- src/routes/seamfix_treatment.py
+++ src/routes/seamfix_treatment.py
@@ -104,103 +104,82 @@ def get_seamfix_treatment_lite(criteria=None, index=0, size=10):
     logging.info(response)
     logging.info("**** End get_seamfix_treatment_lite ****")
     return response
-
-
-# def get_seamfix_treatment_lite():
-#     logging.info("**** Begin get_seamfix_treatment_lite ****")
-#     logging.info("/seamfix_treatment/getByCriteria")
-#     r = request.get_json() or {}
-#     logging.info("**** request input ****")
-#     logging.info(r)
-#     index = r.get('index')
-#     size = r.get('size')
-#     r['data']['status'] = "Untreated"
-#     seamfix_treatments, total_items = SeamfixTreatment.get_by_criteria(r['data'], index, size)
-#     if seamfix_treatments:
-#         message = functional_error.MESSAGE_SUCCESS()
-#     else:
-#         message = functional_error.MESSAGE_DATA_EMPTY()
-#     response = {"items": [seamfix_treatment.as_dict() for seamfix_treatment in seamfix_treatments], "count": total_items, "message": message, "code": 200, "has_error": False}
-#     logging.info("**** response output ****")
-#     logging.info(response)
-#     logging.info("**** End get_user ****")
-#     return response
     
 
-@app.route('/seamfix_treatment/create', methods=['POST'])
-@cross_origin()
-def create_seamfix_treatment():
-    logging.info("**** Begin create_seamfix_treatment ****")
-    print("**** Begin create_seamfix_treatment ****")
-    logging.info("/seamfix_treatment/create")
-    cursor.execute(""" 
-        SELECT TOP (1) 
-            b.[ID],
-            b.[MSISDN],
-            b.[APP],
-            b.[ExecState],
-            b.[ReturnID],
-            p1.Picture AS IDCardFPicturePath,
-            p2.Picture AS IDContractPicturePath,
-            p3.Picture AS IDFrontPicturePath
-        FROM [SIMRegistration].[dbo].[SIMRegistrationQueue] b WITH (NOLOCK)
-        LEFT JOIN [SIMRegistration].[dbo].[SIMRegistrationPictures] p1 WITH (NOLOCK)
-            ON p1.ID = b.IDCardFPicture
-        LEFT JOIN [SIMRegistration].[dbo].[SIMRegistrationPictures] p2 WITH (NOLOCK)
-            ON p2.ID = b.IDContractPicture
-        LEFT JOIN [SIMRegistration].[dbo].[SIMRegistrationPictures] p3 WITH (NOLOCK)
-            ON p3.ID = b.IDFrontPicture
-        ORDER BY b.ID DESC;
-     """)
-    rows = cursor.fetchall()
-    for row in rows:
-        msisdn = str(row[1])  # Assurez-vous que ce soit une string
-        def safe_image_save(binary_data, prefix):
-            if binary_data:
-                try:
-                    base64_str = base64.b64encode(binary_data).decode("utf-8")
-                    return utilities.save_base64_image_lite(base64_str, f"{prefix}_{msisdn}")
-                except Exception as e:
-                    logging.error(f"Erreur lors de la sauvegarde de l'image {prefix}: {e}")
-            return None
+# @app.route('/seamfix_treatment/create', methods=['POST'])
+# @cross_origin()
+# def create_seamfix_treatment():
+#     logging.info("**** Begin create_seamfix_treatment ****")
+#     print("**** Begin create_seamfix_treatment ****")
+#     logging.info("/seamfix_treatment/create")
+#     cursor.execute(""" 
+#         SELECT TOP (1) 
+#             b.[ID],
+#             b.[MSISDN],
+#             b.[APP],
+#             b.[ExecState],
+#             b.[ReturnID],
+#             p1.Picture AS IDCardFPicturePath,
+#             p2.Picture AS IDContractPicturePath,
+#             p3.Picture AS IDFrontPicturePath
+#         FROM [SIMRegistration].[dbo].[SIMRegistrationQueue] b WITH (NOLOCK)
+#         LEFT JOIN [SIMRegistration].[dbo].[SIMRegistrationPictures] p1 WITH (NOLOCK)
+#             ON p1.ID = b.IDCardFPicture
+#         LEFT JOIN [SIMRegistration].[dbo].[SIMRegistrationPictures] p2 WITH (NOLOCK)
+#             ON p2.ID = b.IDContractPicture
+#         LEFT JOIN [SIMRegistration].[dbo].[SIMRegistrationPictures] p3 WITH (NOLOCK)
+#             ON p3.ID = b.IDFrontPicture
+#         ORDER BY b.ID DESC;
+#      """)
+#     rows = cursor.fetchall()
+#     for row in rows:
+#         msisdn = str(row[1])  # Assurez-vous que ce soit une string
+#         def safe_image_save(binary_data, prefix):
+#             if binary_data:
+#                 try:
+#                     base64_str = base64.b64encode(binary_data).decode("utf-8")
+#                     return utilities.save_base64_image_lite(base64_str, f"{prefix}_{msisdn}")
+#                 except Exception as e:
+#                     logging.error(f"Erreur lors de la sauvegarde de l'image {prefix}: {e}")
+#             return None
             
-        id_card_picture_path = safe_image_save(row[5], "id_card_picture_path")
-        id_contrat_picture_path = safe_image_save(row[6], "id_contrat_picture_path")
-        id_front_picture_path = safe_image_save(row[7], "id_front_picture_path")
-        seamfix_treatment = SeamfixTreatment(
-            msisdn=msisdn,
-            id_card_picture_path=id_card_picture_path,
-            id_contrat_picture_path=id_contrat_picture_path,
-            id_front_picture_path=id_front_picture_path,
-            status="Untreated",
-            created_at=datetime.now(),
-            # search_string=utilities.build_search_string(row),
-            is_deleted=False,
-        )
-        db.session.add(seamfix_treatment)
-        db.session.commit()
+#         id_card_picture_path = safe_image_save(row[5], "id_card_picture_path")
+#         id_contrat_picture_path = safe_image_save(row[6], "id_contrat_picture_path")
+#         id_front_picture_path = safe_image_save(row[7], "id_front_picture_path")
+#         seamfix_treatment = SeamfixTreatment(
+#             msisdn=msisdn,
+#             id_card_picture_path=id_card_picture_path,
+#             id_contrat_picture_path=id_contrat_picture_path,
+#             id_front_picture_path=id_front_picture_path,
+#             status="Untreated",
+#             created_at=datetime.now(),
+#             # search_string=utilities.build_search_string(row),
+#             is_deleted=False,
+#         )
+#         db.session.add(seamfix_treatment)
+#         db.session.commit()
 
-        card_picture = binary_to_base64(row[5])
-        front_picture = binary_to_base64(row[7])
-        # on call le getByCriteria
-        get_response = get_seamfix_treatment_lite()
-        # on declanche le l'orchestration seamfix treatment
-        orchestration_seamfix_treatment(front_picture, card_picture, "passport", "png")
-        # Lancement asynchrone de l’orchestration
-        # def run_async_orchestration(front_picture, card_picture, doc_type, doc_format):
-        #     try:
-        #         orchestration_seamfix_treatment(front_picture, card_picture, doc_type, doc_format)
-        #     except Exception as e:
-        #         logging.error(f"Erreur dans orchestration_seamfix_treatment async : {e}")
+#         card_picture = binary_to_base64(row[5])
+#         front_picture = binary_to_base64(row[7])
+#         # on call le getByCriteria
+#         get_response = get_seamfix_treatment_lite()
+#         # on declanche le l'orchestration seamfix treatment
+#         orchestration_seamfix_treatment(front_picture, card_picture, "passport", "png")
+#         # Lancement asynchrone de l’orchestration
+#         # def run_async_orchestration(front_picture, card_picture, doc_type, doc_format):
+#         #     try:
+#         #         orchestration_seamfix_treatment(front_picture, card_picture, doc_type, doc_format)
+#         #     except Exception as e:
+#         #         logging.error(f"Erreur dans orchestration_seamfix_treatment async : {e}")
 
-        # threading.Thread(
-        #     target=run_async_orchestration,
-        #     args=(front_picture, card_picture, "passport", "png"),
-        #     daemon=True
-        # ).start()   
-    logging.info("**** End create_seamfix_treatment ****")
-    print("**** End create_seamfix_treatment ****")
-    return get_response
+#         # threading.Thread(
+#         #     target=run_async_orchestration,
+#         #     args=(front_picture, card_picture, "passport", "png"),
+#         #     daemon=True
+#         # ).start()   
+#     logging.info("**** End create_seamfix_treatment ****")
+#     print("**** End create_seamfix_treatment ****")
+#     return get_response
 
 
 def binary_to_base64(binary_data):
