07dd7f59b080c3a6ec2776c23a81b7473efada22
kouadio <innocent.kouadio>
2025-06-06 07:39:41 +0000

    MAJ: gestion des logs

--- src/routes/seamfix_treatment.py
+++ src/routes/seamfix_treatment.py
@@ -147,7 +147,8 @@ def orchestration_seamfix_treatment(front_picture, document, documentType, docum
         data = ocr.get("data", {})
         if data:
             extractedDataList = data.get("extractedDataList", [])
-            item = extractedDataList[11]
+            item = extractedDataList[-1]
+            logging.info(f"Dernier élément extrait : {item}")
             image = item.get("value")
             # on call le face matching
             portrait_seamfix_verify_lite(front_picture, image)
